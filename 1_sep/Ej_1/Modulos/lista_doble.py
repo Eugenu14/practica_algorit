508345729f2ca7cb0922bf5b976793c136232112
NazaEugen <eu.ullmann@gmail.com>
2022-09-01 13:01:23 -0300

    nuevos cambios

--- 1_sep/Ej_1/Modulos/lista_doble.py
+++ 1_sep/Ej_1/Modulos/lista_doble.py
@@ -61,8 +61,43 @@ class Lista_DE :
             nodo.anterior = self.cola
             self.cola.siguiente = nodo
             self.cola = nodo
+            
+        self.tamanio += 1
+    
+    def insertar (self,posicion,dato):
+        temp = self.cabeza
+        nodo = Nodo(dato)
         
+        if posicion > 0 and not posicion== self.tamanio:            
+            for i in range(posicion-1):
+                temp = temp.siguiente
+            nodo.siguiente = temp.siguiente
+            temp.siguiente.anterior = nodo
+            temp.siguiente = nodo
+            nodo.anterior = temp
+            self.tamanio += 1
+            
+        elif posicion == 0:
+            self.agregar(dato)
         
+        elif posicion == self.tamanio :
+            self.anexar(dato)
+        
+        elif posicion == self.tamanio-1:
+            self.cola.anterior.siguiente = nodo
+            nodo.anterior = self.cola.anterior
+            nodo.siguiente = self.cola
+            self.cola.anterior = nodo
+            self.tamanio +=1
+        
+        elif posicion < 0 or posicion > self.tamanio :
+            print ("Fuera de rango")
+            
+            
+        
+            
+            
+            
         
         
         
