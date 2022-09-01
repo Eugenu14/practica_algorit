3394af4b517b531c450bd08eca8537407910a43b
NazaEugen <eu.ullmann@gmail.com>
2022-09-01 13:02:49 -0300

    a

--- 1_sep/Ej_1/Modulos/nodo.py
+++ 1_sep/Ej_1/Modulos/nodo.py
@@ -5,3 +5,36 @@ Created on Thu Sep  1 10:58:34 2022
 @author: alumno
 """
 
+class Nodo:
+    def __init__(self,datoInicial):
+        self.dato = datoInicial
+        self.siguiente = None
+        self.anterior = None
+      
+    
+    @property
+    def dato(self):
+        return self._dato
+    @property
+    def siguiente(self):
+        return self._siguiente
+    
+    @dato.setter
+    def dato(self,nuevodato):
+        self._dato = nuevodato
+        
+    @siguiente.setter
+    def siguiente(self,nuevosiguiente):
+        self._siguiente = nuevosiguiente
+        
+    @property
+    def anterior(self):
+         return self._anterior
+
+    @anterior.setter
+    def anterior(self,nuevo):
+        self._anterior = nuevo
+        
+
+        
+    
